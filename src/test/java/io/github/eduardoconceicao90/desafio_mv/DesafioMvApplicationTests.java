969802e6c5750b73edc3279e2a2e74bc74fa6065
Eduardo Conceição <eduardosaconceicao@gmail.com>
2024-09-05 14:49:59 -0300

    first commit

---
+++ src/test/java/io/github/eduardoconceicao90/desafio_mv/DesafioMvApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eduardoconceicao90.desafio_mv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
